d6acab47918b3656ca5e0f9cff38d0cb11c85608
Yurin Matvey <murin8102@gmail.com>
2026-03-24 10:42:42 +0300

    second commit

--- django_app/accounts/admin.py
+++ django_app/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','grade','target_score','created_at')
+    search_fields = ('user__username','user__first_name','user__last_name','patronymic')
+    list_filter = ('grade',)
+    readonly_fields=('created_at','updated_at')
+
+
