37c4c37460464a9aa2b15055d98d7986acb94e39
lasofiko <la.sofaa@yandex.ru>
2026-03-24 23:05:48 +0300

    add user authentication with JWT and UserProfile model

--- django_app/accounts/admin.py
+++ django_app/accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [UserProfileInline]
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_grade')
+
+    def get_grade(self, obj):
+        return obj.profile.grade if hasattr(obj, 'profile') else '-'
+
+    get_grade.short_description = 'Класс'
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "grade", "phone")
+    search_fields = ("user__username", "user__first_name", "user__last_name")
+    list_filter = ("grade",)
