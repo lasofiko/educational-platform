2f88238d53f18ca2e857a8206b82726f86cf1990
Yurin Matvey <murin8102@gmail.com>
2026-03-26 00:18:22 +0300

    enrollment and UserProgress

--- django_app/progress/admin.py
+++ django_app/progress/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserProgress,Enrollment
 
-# Register your models here.
+@admin.register(UserProgress)
+class UserProgressAdmin(admin.ModelAdmin):
+    list_display = ('user','node','status','score','created_at')
+    list_filter = ('status',)
+    readonly_fields = ('completed_at','created_at','updated_at')
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display=('user','subject','enrolled_at')
+    list_filter = ('subject',)
+    readonly_fields = ('enrolled_at',)
