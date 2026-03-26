e83f7abd1a9bc3112a3f7509b248a667d48a36dd
Fraimy1_wsl <a.baxtiozinn@gmail.com>
2026-03-26 18:18:07 +0300

    [add] Subject model courses/models.py

--- django_app/courses/models.py
+++ django_app/courses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название предмета")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    icon = models.CharField(max_length=50, blank=True, verbose_name="Иконка")
+
+    class Meta:
+        verbose_name = "Предмет"
+        verbose_name_plural = "Предметы"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
