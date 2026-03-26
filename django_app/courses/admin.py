eae7ba7ef538c048c671334fef51b4eb85b47c0e
Baobab909 <rita.shcherbakova07@gmail.com>
2026-03-26 15:29:36 +0300

    добавлена админка на модель предмета

--- django_app/courses/admin.py
+++ django_app/courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Subject, RoadmapNode, Lesson, Task
 
-# Register your models here.
+admin.site.register(Subject)
+admin.site.register(RoadmapNode)
+admin.site.register(Lesson)
+admin.site.register(Task)
