87a7a80a430cead38cc21099799efc1ce8386313
lasofiko <la.sofaa@yandex.ru>
2026-03-26 22:50:29 +0300

    Docker, docker-compose

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -1,10 +1,3 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-"""
-
 from django.contrib import admin
 from django.urls import include, path
 
