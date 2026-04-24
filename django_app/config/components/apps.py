9b0904a053802815d956cdcd183b1084ed3af7a8
Матвей Юрин <matveyyuri@MacBook-Pro-Matvej.local>
2026-04-24 22:50:14 +0300

    commit before merge

--- django_app/config/components/apps.py
+++ django_app/config/components/apps.py
@@ -16,3 +16,14 @@ INSTALLED_APPS = [
     "accounts",
     "progress",
 ]
+
+
+
+INSTALLED_APPS += [
+    'django_app.common',
+]
+
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'django_app.common.exception_handler.common_exception_handler',
+}
